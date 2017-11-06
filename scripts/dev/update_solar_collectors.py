776a2e8cb9f3cc363142a058be9bc5e09e720ed8
Matt Steen <msteen@ambient-e.com>
2017-11-05 20:32:03 -0700

    update_solar_collectors script for #6378

---
+++ scripts/dev/update_solar_collectors.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+from eppy.modeleditor import IDF
+
+
+def update_solar_collectors(csv_path, idd_dir):
+  
+    # eppy setup
+    idd_name = r'Energy+.idd'
+    idd_path = os.path.join(idd_dir, idd_name)
+    IDF.setiddname(idd_path)
+
+    # create a blank idf, add Version object
+    idf_dir = csv_dir
+    idf_name = r'SolarCollectors2017.idf'
+    idf_path = os.path.join(idf_dir, idf_name)
+    open(idf_path, 'w').close()
+    idf = IDF(idf_path)
+    idf.newidfobject('VERSION')
+    idf.save()
+
+    counter = 0
+
+    df = pd.read_csv(csv_path)
+
+    for idx, row in df.iterrows():
+
+        if idx <= 1 or row[2] == 'Tubular':
+
+            next
+
+        else:
+
+            name = row[0]
+            srcc_type = row[2]
+            test_flow = row[4]
+            area = row[5]
+            frta = row[6]
+            frul = row[7]
+            iam = row[8]
+
+            obj = idf.newidfobject('SolarCollectorPerformance:FlatPlate'.upper())
+            obj.Name = name
+            obj.Gross_Area = area
+            obj.Test_Fluid = 'Water'
+            obj.Test_Flow_Rate = float(test_flow)/1000 #kg/s to m3/s
+            obj.Test_Correlation_Type = 'Inlet'
+            obj.Coefficient_1_of_Efficiency_Equation = frta
+            obj.Coefficient_2_of_Efficiency_Equation = frul
+            obj.Coefficient_3_of_Efficiency_Equation = 0
+            obj.Coefficient_2_of_Incident_Angle_Modifier = iam
+            obj.Coefficient_3_of_Incident_Angle_Modifier = 0
+
+            counter += 1
+
+    idf.save()
+
+    print counter     
+  
